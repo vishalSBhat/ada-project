a59acfd3fb3d38bf2d5ce36bade7bc83f8d620da
vishalSBhat <vishalbhat97@gmail.com>
2020-05-05 12:54:11 +0530

    Updates

---
+++ precache-manifest.1a8502e29acb1beec0b9f5833bb85a77.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdebdff87cd1234820d7651d9df2f14f",
+    "url": "/ada-project/index.html"
+  },
+  {
+    "revision": "4e18a07aa71f273c05eb",
+    "url": "/ada-project/static/css/main.adbf180b.chunk.css"
+  },
+  {
+    "revision": "c079d629cfb1f0ea3cab",
+    "url": "/ada-project/static/js/2.aba98b59.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ada-project/static/js/2.aba98b59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e18a07aa71f273c05eb",
+    "url": "/ada-project/static/js/main.09eeffb3.chunk.js"
+  },
+  {
+    "revision": "0d0837c78f0c357ba0a4",
+    "url": "/ada-project/static/js/runtime-main.423e389c.js"
+  },
+  {
+    "revision": "c8b2f7af34c1a104018cf7962275fd94",
+    "url": "/ada-project/static/media/error.c8b2f7af.jpg"
+  },
+  {
+    "revision": "9549a8709ef75ef572ac4bc3fb2a8bc4",
+    "url": "/ada-project/static/media/logo.9549a870.jpg"
+  }
+]);
